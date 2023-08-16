b65370d6c750e8da95b1d9e59ed4e35686e12d50
Davide Colturi <davide.colturi@strong-it.at>
2023-08-16 16:40:57 +0200

    fix DB migration scripts for table results

--- db/db_sqlite3/migrations/20230814000000_0.12.0_targets_attributes.sql
+++ db/db_sqlite3/migrations/20230814000000_0.12.0_targets_attributes.sql
@@ -10,6 +10,15 @@ ALTER TABLE targets ADD COLUMN "phone" varchar(255);
 ALTER TABLE targets ADD COLUMN "degree" varchar(255);
 ALTER TABLE targets ADD COLUMN "desc" varchar(255);
 
+ALTER TABLE results ADD COLUMN "department" VARCHAR(255);
+ALTER TABLE results ADD COLUMN "dep_number" VARCHAR(255);
+ALTER TABLE results ADD COLUMN "age" VARCHAR(255);
+ALTER TABLE results ADD COLUMN "gender" VARCHAR(255);
+ALTER TABLE results ADD COLUMN "site" VARCHAR(255);
+ALTER TABLE results ADD COLUMN "phone" VARCHAR(255);
+ALTER TABLE results ADD COLUMN "degree" VARCHAR(255);
+ALTER TABLE results ADD COLUMN "desc" VARCHAR(255);
+
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
 
